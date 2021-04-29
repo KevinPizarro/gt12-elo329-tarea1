09f3bde9c028e59ee3b8aa3c0833a4eabd80c140
Migelgo <miguel.andrader@sansano.usm.cl>
2021-04-28 22:55:23 -0400

    Completado Simulador

--- Stage1HelpCode/Simulador.java
+++ Stage1HelpCode/Simulador.java
@@ -1,10 +1,12 @@
 import java.io.PrintStream;
+import java.io.File;
+import java.io.FileWriter;
 
 public class Simulador {
     private Comuna comuna;
     private PrintStream out;
 
-    private Simulador(){ }
+    //private Simulador(){ }
     public Simulador (PrintStream output, Comuna comuna){
         out=output;
         this.comuna = comuna;
@@ -13,12 +15,28 @@ public class Simulador {
         String s="time,\t"+Comuna.getStateDescription();
         out.println(s);
     }
-    private void printState(double t){
+    /* private void printState(double t){
         String s = t + ",\t";
         s+= comuna.getState();
         out.println(s);
+    } */
+    public void printxt(String s){
+        File file = new File("salida.csv");
+        try {
+            file.createNewFile();
+        }
+        catch (Exception e){
+            e.getStackTrace();
+        }
+        try {
+            FileWriter output = new FileWriter("salida.csv");
+        output.write(s);
+        output.close();
+        }
+        catch (Exception e){
+            e.getStackTrace();
+        }
     }
-
     /**
      * @param delta_t time step
      * @param endTime simulation time
@@ -26,14 +44,16 @@ public class Simulador {
      */
     public void simulate (double delta_t, double endTime, double samplingTime) {  // simulate time passing
         double t=0;
+        String s = "";
         printStateDescription();
-        printState(t);
+        s = t + ",\t" + comuna.getState() + '\n';
         while (t<endTime) {
             for(double nextStop=t+samplingTime; t<nextStop; t+=delta_t) {
                 comuna.computeNextState(delta_t); // compute its next state based on current global state
                 comuna.updateState();            // update its state
             }
-            printState(t);
+            s += t + ",\t" + comuna.getState() + '\n';
         }
+        printxt(s);
     }
 }
