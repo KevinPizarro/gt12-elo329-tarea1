2a9cef83845163ab37ba806c7e2df764fe117558
Migelgo <miguel.andrader@sansano.usm.cl>
2021-04-28 22:54:43 -0400

    Completados los Metodos de Individuo

--- Stage1HelpCode/Individuo.java
+++ Stage1HelpCode/Individuo.java
@@ -6,6 +6,11 @@ public class Individuo {
     public Individuo (Comuna comuna, double speed, double deltaAngle){
 	//??
         angle = Math.random()*2*Math.PI;
+        x = Math.random()*comuna.getWidth();
+        y = Math.random()*comuna.getHeight();
+        this.speed = speed;
+        this.deltaAngle = deltaAngle; 
+        this.comuna = comuna;
     }
     public static String getStateDescription(){
         return "x,\ty";
@@ -15,18 +20,30 @@ public class Individuo {
     }
     public void computeNextState(double delta_t) {
         double r=Math.random();
-        angle+= //??
-        x_tPlusDelta=x+speed*Math.cos(angle);
+        angle+= Math.floor(r*(2*deltaAngle+1)-deltaAngle); //se suma un valor aleatorio entre -deltaAngle y +deltaAngle
+        x_tPlusDelta=x+speed*Math.cos(angle)*delta_t;
+        y_tPlusDelta=y+speed*Math.sin(angle)*delta_t;
 	//??
         if(x_tPlusDelta < 0){   // rebound logic
-           //??
+            angle = Math.PI - angle;
+            x_tPlusDelta=x+speed*Math.cos(angle)*delta_t;
         }
         else if( x_tPlusDelta > comuna.getWidth()){
-           //??
+            angle = Math.PI - angle;
+            x_tPlusDelta=x+speed*Math.cos(angle)*delta_t;
+        }
+        if(y_tPlusDelta < 0){   // rebound logic
+            angle = 2*Math.PI - angle;
+            y_tPlusDelta=y+speed*Math.sin(angle)*delta_t;
+         }
+        else if( y_tPlusDelta > comuna.getHeight()){
+            angle = 2*Math.PI - angle;
+            y_tPlusDelta=y+speed*Math.sin(angle)*delta_t;
         }
 	//??
     }
     public void updateState(){
-	//??
+	x = x_tPlusDelta;
+    y = y_tPlusDelta;
     }
 }
