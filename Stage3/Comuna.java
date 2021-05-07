36e15c92674b7d11a369176b3618394b470e781b
Pablo_t <pablo.troncosop@sansano.usm.cl>
2021-05-07 16:44:31 -0400

    Se modifica el metodo setPerson para considerar a las personas con mascarillas y el metodo conputeNextState para que considere probabilidades p0 p1 y p2.

--- Stage3/Comuna.java
+++ Stage3/Comuna.java
@@ -1,9 +1,7 @@
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
-import java.math.*;
 
 public class Comuna {
-    private Individuo person;
     private Rectangle2D territory; // Alternatively: double width, length;
     private ArrayList<Individuo> personas;
 
@@ -20,34 +18,49 @@ public class Comuna {
     public double getHeight() {
         return territory.getHeight(); //??
     }
-    public void setPerson(int n_ind, double time_recovery, int n_inf, Comuna comuna, double speed, double deltaAngle){
+    public void setPerson(int n_ind, double time_recovery, int n_inf, Comuna comuna, double speed, double deltaAngle, double m){
         personas = new ArrayList<Individuo>(n_ind);
-        for(int i=0;i<n_ind;i++)
-        {
-            if(i<n_inf)
-            {
+        for(int i=0;i<n_ind;i++){
+            if(i<n_inf){
                 Individuo p=new Individuo(comuna, speed, deltaAngle);
                 personas.add(p);
                 personas.get(i).infect(time_recovery);
+                if(i<n_inf*m){
+                    personas.get(i).putmask();
+                }
             }
-            else
-            {
+            else{
                 Individuo p=new Individuo(comuna, speed, deltaAngle);
                 personas.add(p);
+                if(i-n_inf<(n_ind-n_inf)*m){
+                    personas.get(i).putmask();
+                }
             }
         }
     }
-    public void computeNextState (double delta_t, double distancia, double p0, double rec_time) {
+    public void computeNextState (double delta_t, double distancia, double p0, double p1, double p2, double rec_time) {
         for (int i=0;i < personas.size();i++){
             for(int j=0; j< personas.size();j++){
                 if(personas.get(i).getState()==State.S && personas.get(j).getState()==State.I){
                     double e = Math.sqrt(Math.pow(personas.get(i).getX()-personas.get(j).getX(),2)+Math.pow(personas.get(i).getY()-personas.get(j).getY(),2));
                     if(e < distancia){
-                        if(Math.random()<=p0)
-                        {
-                            personas.get(i).infect(rec_time);
+                        if((personas.get(i).getmask() == true) && (personas.get(j).getmask() == true)){
+                            if(Math.random()<=p2){
+                                personas.get(i).infect(rec_time);
+                            }
                         }
-
+                        else if(((personas.get(i).getmask() == true) && (personas.get(j).getmask() == false)) || ((personas.get(i).getmask() == false) && (personas.get(j).getmask() == true))){
+                            if(Math.random()<=p1){
+                                personas.get(i).infect(rec_time);
+                            }
+                        } 
+                        else{
+                            if(Math.random()<=p0){
+                                personas.get(i).infect(rec_time);
+                            }
+                        }
+                        
+                        
                     }
                 }
             }
