451178e31a7a00260416f1ea8912bc67522a970e
Pablo_t <pablo.troncosop@sansano.usm.cl>
2021-05-07 20:36:03 -0400

    Se agregan comentarios a los archivos comuna de todas las etapas.

--- Stage2/src/Comuna.java
+++ Stage2/src/Comuna.java
@@ -2,24 +2,59 @@ import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.math.*;
 
+
+/**
+ * La clase comuna se encarga de crear el arreglo de las personas y posicionarlas en la comuna, se inicializa
+ * el tamaño de la comuna y se calcula el siguiente estado de los individuos, calculando cuando una persona se infecta.
+ */
 public class Comuna {
     private Individuo person;
     private Rectangle2D territory; // Alternatively: double width, length;
     private ArrayList<Individuo> personas;
 
+    /**
+     * Si no se ingresan parametros se crea un rectangulo con medidas fijas que corresponde a la comuna.
+     */
     public Comuna(){
         territory = new Rectangle2D.Double(0, 0, 1000, 1000); // 1000x1000 m²;
     }
+
+    /**
+     * Se crea la comuna como un rectangulo con los parametros ingresados.
+     * @param width Ancho de la comuna
+     * @param length Largo de la comuna
+     */
     public Comuna(double width, double length){
         territory = new Rectangle2D.Double(0,0, width, length);
 
     }
+
+    /**
+     *
+     * @return Retorna el ancho de la comuna.
+     */
     public double getWidth() {
         return territory.getWidth(); //??
     }
+
+    /**
+     *
+     * @return Retorna el alto de la comuna.
+     */
     public double getHeight() {
         return territory.getHeight(); //??
     }
+
+    /**
+     * Se crea el array de las personas existentes en las comunas y se inicializa la cantidad correspondientes de
+     * personas infectadas.
+     * @param n_ind El número de individuos
+     * @param time_recovery El tiempo de recuperación de las personas.
+     * @param n_inf El número de infectados permitidos.
+     * @param comuna La comuna en la que vive la persona.
+     * @param speed La velocidad de las personas.
+     * @param deltaAngle La variación del angulo donde la persona se desplazará aleatoriamente.
+     */
     public void setPerson(int n_ind, double time_recovery, int n_inf, Comuna comuna, double speed, double deltaAngle){
         personas = new ArrayList<Individuo>(n_ind);
         for(int i=0;i<n_ind;i++)
@@ -37,6 +72,14 @@ public class Comuna {
             }
         }
     }
+
+    /**
+     * Se cácula el siguiente estado de todas las personas en la comuna identificando las personas que se infectan.
+     * @param delta_t La variación del tiempo existente entre caca calculo de estado.
+     * @param distancia La distancia a la cual una eprsona se puede infectar con otra.
+     * @param p0 Probabilidad de infección cuando ninguna de las dos personas usa mascarillas.
+     * @param rec_time Tiempo de recuperación.
+     */
     public void computeNextState (double delta_t, double distancia, double p0, double rec_time) {
         for (int i=0;i < personas.size();i++){
             for(int j=0; j< personas.size();j++){
@@ -57,6 +100,10 @@ public class Comuna {
         }
         
     }
+
+    /**
+     * Se actualiza el estados de todas las personas.
+     */
     public void updateState () {
 
         for(int i=0; i<personas.size();i++) {
@@ -64,6 +111,10 @@ public class Comuna {
         }
     }
 
+    /**
+     *
+     * @return Se retorna la cantidad de recuperados, infectados y susceptibles.
+     */
     public String getIndState()
     {
         int sus=0,inf=0,rec=0;
